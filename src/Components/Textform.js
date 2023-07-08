96d9f33ace70f38a894859279e2350882cbf7454
indian_biber <rupeshbharatmore@gmail.com>
2023-07-08 13:32:18 +0530

    Video no 8 done which involved usage of color change navigation bar using bg-ligh and button color change and addition of lowercase

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -6,6 +6,11 @@ export default function Textform(props) {
         setText(newtext);
     }
 
+    const handlelowclick = ()=>{
+        let newtext = text.toLowerCase();
+        setText(newtext);
+    }
+
     const handleonchange = (event)=>{
         let m = event.target.value
         setText(m)
@@ -14,16 +19,27 @@ export default function Textform(props) {
     
     const [text,setText] = useState("enter text here");
   return (
-        <div className="mb-3">
+    <>
+        <div className="container">
             
             <h1>{props.title}</h1>
             <div className='mb-3'>
             <textarea className="form-control" value = {text} onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
-            <button className='btn btn-primary' onClick={handleclick}>
+            <button className='btn btn-primary mx-1' onClick={handleclick}>
                 Convert to upper case
             </button>
+            <button className='btn btn-primary mx-1 ' onClick={handlelowclick}>
+                Convert to Lower case
+            </button>
         </div>
-    
+        <div className='container'>
+            <h2>Your Text Sumary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").length*60} seconds reuired to read above content </p>
+            <h3>Preview</h3>
+            <p>{text}</p>
+        </div>
+        </>
   )
 }
